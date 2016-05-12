f9c419dbc34a47fc53173404d2ef0b95180935ed
joonatan samuel <jonksar@github.com>
2016-05-12 16:39:04 +0300

    Added initial repo

---
+++ Assets/Prefabs/Ambient Music/AmbientSound.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmbientSound : MonoBehaviour {
+
+	public AudioSource audio;
+	[Range(0f, 1f)][SerializeField]public float dt = 0.1f;
+
+	void Start(){
+		InvokeRepeating("PlaySound",0.001f, audio.clip.length + Random.value * dt * audio.clip.length);
+	}
+
+	void PlaySound(){
+		audio.pitch = 1f + 0.3f * Random.value;
+		audio.Play();
+	}
+}
---
+++ Assets/Prefabs/BWController/Lights.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Lights : MonoBehaviour {
+	public List<GameObject> torchesInRange;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider c) {
+		Debug.Log (c.gameObject.tag);
+		if (c.gameObject.tag == "Torch") {
+			torchesInRange.Add(c.gameObject);
+		}
+	}
+
+	void OnTriggerExit(Collider c) {
+		if (c.gameObject.tag == "Torch") {
+			torchesInRange.Remove(c.gameObject);
+		}
+	}
+}
